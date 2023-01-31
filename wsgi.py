109a634fe0fb18d4120fa73102db06e544c7c7b4
OllyYa <exrhodo@gmail.com>
2023-01-31 17:04:04 +0300

    model tags

--- wsgi.py
+++ wsgi.py
@@ -33,10 +33,30 @@ def create_admin():
     """
     from blog.models import User
 
-    admin = User(username="admin_from_terminal", is_staff=True)
+    admin = User(username="admin", is_staff=True)
     admin.password = os.environ.get("ADMIN_PASSWORD") or "adminpass"
 
     db.session.add(admin)
     db.session.commit()
 
     print("created admin:", admin)
+
+
+@app.cli.command("create-tags")
+def create_tags():
+    """
+    Run in your terminal:
+    ➜ flask create-tags
+    """
+    from blog.models import Tag
+    for name in [
+        "flask",
+        "django",
+        "python",
+        "sqlalchemy",
+        "news",
+    ]:
+        tag = Tag(name=name)
+        db.session.add(tag)
+    db.session.commit()
+    print("created tags")
